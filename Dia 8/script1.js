9aee702b97fe44cd61a74ec22103de72fb19f06a
Guilherme Lima <guilhermefk123@gmail.com>
2025-03-24 21:18:12 -0300

    Dia 8

---
+++ Dia 8/script1.js
@@ -0,0 +1,39 @@
+//Soma 
+//Subtração
+//Divisão
+//Multiplicação
+//Exponenciação
+function Calculadora(numero1, numero2, operador) {
+    let resultado;
+    let num1 = parseInt(numero1);
+    let num2 = parseInt(numero2);
+
+    switch (operador) {
+        case "+":
+            resultado = num1 + num2;
+            break;
+        case "-":
+            resultado = num1 - num2;
+            break;
+        case "*":
+            resultado = num1 * num2;
+            break;
+        case "/":
+            resultado = num1 / num2;
+            break;
+        case "exp":
+            let contador = 1;
+            resultado = num1 * num1;
+            while (contador < num2 - 1) {
+                resultado = resultado * num1;
+                contador++;
+            }
+            break;
+    }
+
+    if (resultado == undefined || resultado > 1000000) {
+        resultado = "ERRO";
+    }
+
+    return resultado;
+}
