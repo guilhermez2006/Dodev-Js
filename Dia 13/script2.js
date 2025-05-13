4d2e8cb3793f4c2e44f91a46802b23bfa4c93dd8
Guilherme Lima <guilhermefk123@gmail.com>
2025-05-13 10:41:49 -0300

    Ajustes no ex2 do Dia 13

--- Dia 13/script2.js
+++ Dia 13/script2.js
@@ -1,5 +1,4 @@
 class Carro {
-
     constructor(nome, potencia, velocidadeMaxima, aceleracao) {
         this.nome = nome;
         this.potencia = potencia;
@@ -7,13 +6,62 @@ class Carro {
         this.aceleracao = aceleracao;
     }
     CalcularTempoMedio(distancia) {
-        let resultado = distancia / (this.velocidadeMaxima / this.aceleracao)
+        let resultado = distancia / (this.velocidadeMaxima / this.aceleracao);
         return resultado;
     }
-    ExibirInformacoes() {
-        alert(`Nome do carro: ${this.nome}\nPotência: ${this.potencia}\nVelocidade Máxima: ${this.velocidadeMaxima}\nAceleração: ${this.aceleracao}`)
+}
+const carro1 = new Carro("Ferrari SF90 Stradale", "1000 cv", 340, 2.5);
+const carro2 = new Carro("Porsche 911 GT3 RS", "525 cv", 296, 3.2);
+const carro3 = new Carro("Mercedes-AMG One", "1063 cv", 352, 2.9);
+const carro4 = new Carro("McLaren P1", "916 cv", 350, 2.8);
+
+// Array de objetos da classe Carro
+const listaDeCarros = [carro1, carro2, carro3, carro4];
+
+class Corrida {
+    constructor(nome, tipo, distancia, participantes, vencedor) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.distancia = distancia;
+        this.participantes = participantes;
+        this.vencedor = vencedor;
+    }
+
+    VencedorEmMenorTempo() {
+        if (!this.participantes || this.participantes.length === 0) {
+            alert("Não há participantes na corrida!");
+            return;
+        }
+
+        let vencedorTemporario = null;
+        let menorTempo = Infinity;
+
+        for (const carro of this.participantes) {
+            const tempo = carro.CalcularTempoMedio(this.distancia);
+
+            if (tempo < menorTempo) {
+                menorTempo = tempo;
+                vencedorTemporario = carro;
+            }
+        }
+
+        if (vencedorTemporario) {
+            this.vencedor = vencedorTemporario.nome;
+        } else {
+            this.vencedor = "Nenhum vencedor encontrado.";
+        }
+    }
+
+    ExibirVencedor() {
+        if (this.vencedor) {
+            alert(`O vencedor da corrida "${this.nome}" é: ${this.vencedor}`);
+        } else {
+            alert(`A corrida "${this.nome}" ainda não tem um vencedor definido.`);
+        }
     }
 }
 
-const carro1 = new Carro("Ferrari", "1.000 cv", 400, 4)
-carro1.ExibirInformacoes()
+const corrida = new Corrida("Monâco", "Formula 1", 3377, listaDeCarros);
+
+corrida.VencedorEmMenorTempo();
+corrida.ExibirVencedor();
