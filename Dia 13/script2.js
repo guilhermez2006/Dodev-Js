c8d1274289cbb56dfcdf3204bd4b7e7b813cf20a
Guilherme Lima <guilhermefk123@gmail.com>
2025-05-12 23:12:34 -0300

    Finalizando ex2 do Dia 13

---
+++ Dia 13/script2.js
@@ -0,0 +1,19 @@
+class Carro {
+
+    constructor(nome, potencia, velocidadeMaxima, aceleracao) {
+        this.nome = nome;
+        this.potencia = potencia;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.aceleracao = aceleracao;
+    }
+    CalcularTempoMedio(distancia) {
+        let resultado = distancia / (this.velocidadeMaxima / this.aceleracao)
+        return resultado;
+    }
+    ExibirInformacoes() {
+        alert(`Nome do carro: ${this.nome}\nPotência: ${this.potencia}\nVelocidade Máxima: ${this.velocidadeMaxima}\nAceleração: ${this.aceleracao}`)
+    }
+}
+
+const carro1 = new Carro("Ferrari", "1.000 cv", "400Km/H", 4)
+carro1.ExibirInformacoes()
