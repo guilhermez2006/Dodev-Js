d5497633101d733138b5f5b8e2f4b3be2e328bd2
Guilherme Lima <guilhermefk123@gmail.com>
2025-06-12 21:27:07 -0300

    Finalizando ex1 do Dia22

---
+++ Dia 22/script.js
@@ -0,0 +1,13 @@
+// 1
+const olaMundo = () => "Olá, Mundo!";
+
+// 2
+const dobro = x => x * 2;
+
+// 3
+const mult = (a, b) => a * b;
+console.log(mult(8, 2));
+
+// 4
+let saudacao = nome => `Olá ${nome}, Seja bem vindo!`;
+console.log(saudacao("Gui"));
