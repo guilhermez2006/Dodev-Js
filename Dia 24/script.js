39bf3ff4358b35cca6cb32fca277621b1e28d058
Guilherme Lima <guilhermefk123@gmail.com>
2025-06-13 21:20:16 -0300

    Finalizando ex1 do Dia 24

---
+++ Dia 24/script.js
@@ -0,0 +1,11 @@
+// 1
+const frutas = ["maçã", "banana", "uva"];
+frutas.forEach(function (fruta, i) {
+    console.log(i, fruta);
+});
+
+// 2
+const cores = ["azul", "vermelho", "verde", "amarelo"];
+cores.forEach(function(cor, i){
+    console.log(cor); 
+});
