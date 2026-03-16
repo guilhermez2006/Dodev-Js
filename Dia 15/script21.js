1ab967790648f6ae13844e9b12598023c387a128
Guilherme Lima <guilhermefk123@gmail.com>
2026-03-15 23:22:29 -0300

    Finalizando ex21 do Dia 21

---
+++ Dia 15/script21.js
@@ -0,0 +1,9 @@
+function tamanhoUltimaPalavra(s) {
+let ordenarS = s
+    for (let i = 0; i < s.length; i++) {
+
+    }
+}
+
+let resultado = tamanhoUltimaPalavra("Hello word")
+console.log(resultado)
